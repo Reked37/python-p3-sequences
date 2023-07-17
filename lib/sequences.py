27d2ac47fcf4f71f69527b000aba54651f60bc86
Derek Truesdale <dtruesdale37@gmail.com>
2023-07-17 14:34:00 -0500

    lab completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list=[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    if length == 0:
+        return print(fib_list[length:length])
+    elif length == 1:
+        return print(fib_list[0:length])
+    elif length == 2:
+        return print(fib_list[0:length])
+    else :
+        return print(fib_list[0:length])
